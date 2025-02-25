fd6692d0762502910ef78ae7467ad601ebd96e30
maheshsangewar <maheshcoengg@gmail.com>
2025-02-25 08:07:43 +0530

    I Have Removed data

--- src/Registration/GitTestRegistration.java
+++ src/Registration/GitTestRegistration.java
@@ -1,21 +1,6 @@
 package Registration;
 
 public class GitTestRegistration {
-	
-	GIT Commands
-	git --version
 
-	 git config --global user.email "maheshcoengg@gmail.com"
-	 
-	 git config --global user.name "maheshsangewar"
-	 
-	 git init
-	 git status [ To Track and untrack files ]
-	 git add . -- To add untracked files
-
-	 git commit -m "I Have Created New Class"
-	 
-	 git remote add origin https://github.com/maheshsangewar/mahinikRepo.git
-	 
 
 }
