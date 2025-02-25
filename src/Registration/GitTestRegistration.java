3052e9c15df9cebb4f4970e31758af2edd9b5a23
maheshsangewar <maheshcoengg@gmail.com>
2025-02-25 08:03:53 +0530

    I Have added data

--- src/Registration/GitTestRegistration.java
+++ src/Registration/GitTestRegistration.java
@@ -1,5 +1,21 @@
 package Registration;
 
 public class GitTestRegistration {
+	
+	GIT Commands
+	git --version
+
+	 git config --global user.email "maheshcoengg@gmail.com"
+	 
+	 git config --global user.name "maheshsangewar"
+	 
+	 git init
+	 git status [ To Track and untrack files ]
+	 git add . -- To add untracked files
+
+	 git commit -m "I Have Created New Class"
+	 
+	 git remote add origin https://github.com/maheshsangewar/mahinikRepo.git
+	 
 
 }
